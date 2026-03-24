9ecb506a8be13887ee4985bfd007cd4850ff455e
westhoon601-a11y <westhoon601@gmail.com>
2026-03-24 09:44:34 +0900

    Add files via upload

--- streamlit-chatbot.py
+++ streamlit-chatbot.py
@@ -3,28 +3,40 @@ import streamlit as st
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    print(openai_api_key)
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+
+#대화내용을 기록할 messages변수를 초기화
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+#대화내용을 화면에 출력
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
+#사용자 입력(질문)을 대기
 if prompt := st.chat_input():
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
+    #사용자 입력(질문)을 대화 내용에 기록하고 화면에 출력
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+
+    #LLM 질의 및 응답 수신
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=st.session_state.messages
+    )
     msg = response.choices[0].message.content
+    #LLM의 답변을 대화내용에 기록하고 화면에 출력
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
 
